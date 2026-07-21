1d0fb10d2f220c50060a70fa2428053e4ba4cd2d
Vgaikwad2513 <gaikwadvaishnavi903@gmail.com>
2026-07-22 03:10:55 +0530

    streamlit link added for backend link

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import requests
 import streamlit as st
 
 # =============================
-# CONFIG
+# CONFIG  
 # =============================
-API_BASE = "https://movie-recommendation-bj5u.onrender.com" or "http://127.0.0.1:8000"
+API_BASE = "https://movie-recommendation-system-otl6.onrender.com" or "http://127.0.0.1:8000"
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
 
 st.set_page_config(page_title="Movie Recommender", page_icon="🎬", layout="wide")
